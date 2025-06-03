529dab2c65a7f5a1311afe642bf8622616b26cc4
Matheus Kitamura <matheusgurther@outlook.com>
2025-06-03 20:38:23 -0300

    Criando o primeiro slideshow

---
+++ Assets/script.js
@@ -0,0 +1,15 @@
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+function nextImage(){
+    count++;
+    if(count > 2){
+        count = 1;
+    }
+
+    document.getElementById("radio" + count).checked = true;
+}
+
+setInterval( function(){
+    nextImage();
+}, 2000)
