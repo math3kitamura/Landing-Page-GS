d50984e024f4eba5186289e4c76c80f2cffabe11
Matheus Kitamura <matheusgurther@outlook.com>
2025-06-03 22:07:23 -0300

    Criando validacao primeiro campo

--- Assets/script.js
+++ Assets/script.js
@@ -12,4 +12,33 @@ function nextImage(){
 
 setInterval( function(){
     nextImage();
-}, 2000)
+}, 2500)
+
+const form = document.getElementById('form');
+const local = document.getElementById('local');
+const data = document.getElementById('data');
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    
+    checkInputLocal()
+})
+
+function checkInputLocal(){
+    const localValue = local.value;
+
+    if(localValue === ""){
+        errorInput(local, "Você precisa preencher este campo")
+    } else{
+        const formItem = local.parentElement;
+        formItem.classList = "form-content"
+    }
+}
+
+function errorInput(input, message) {
+    const formItem = input.parentElement;
+    const textMessage = formItem.querySelector(".error-message")
+
+    textMessage.innerText = message;
+    formItem.className = "form-content error";
+}
